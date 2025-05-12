7be35830b10f9d397d6909cc2ebafa2abadfabbe
Aziz <aziz@humanitec.com>
2025-05-12 11:12:41 +0200

    update yarn commands

--- plugins/humanitec/src/components/HumanitecErrorState.tsx
+++ plugins/humanitec/src/components/HumanitecErrorState.tsx
@@ -1,6 +1,5 @@
 import { Typography } from '@material-ui/core';
 
-
 export function HumanitecErrorState({ error }: { error: Error }) {
   return <Typography color="error">{error.message}</Typography>
 }
